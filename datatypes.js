ebe7a6063704188618057b56b0eb57a6a84bbe29
Fidelis Manyara <manyarafidelis9.com>
2025-05-20 23:47:42 +0300

    added 'created by Driller' and updated BMI page

--- datatypes.js
+++
@@ -1,6 +0,0 @@
-let x = 4;
-console.log(typeof x);
-let y = true;
-let z= '4';
-console.log(typeof y)
-console.log(typeof z)
--- fm.py
+++
@@ -1,6 +0,0 @@
-fruit = ( 'apple' ,'orange' , 'lemon')
-fruitList = list(fruit)
-fruitList[1] = "banana"
-fruit = tuple(fruitList)
-print (type(fruit))
- 
